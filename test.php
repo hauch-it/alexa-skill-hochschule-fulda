a8902f4eeee7630e993a66d2079c2aef7b4b897f
Unknown <d.stemmer92@gmail.com>
2017-12-05 18:58:41 +0100

    fixed the Function Name to test the JsonOutput

--- test.php
+++ test.php
@@ -5,6 +5,6 @@ require_once('alexa.php');
 $mensa = new MensaClass();
 
 $alexa = new AlexaClass();
-$jsonOut = $alexa->alexaJsonOutTitle($mensa->filter('2017-12-05', 'fulda', 'vegetarisch'));
+$jsonOut = $alexa->alexaJsonOutTitleAfterFilter($mensa->filter('2017-12-05', 'fulda', 'vegetarisch'));
 
 ?>
