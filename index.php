2784af8ef92e025cae4954e82e49750521f7a747
Nicolai Ernst <nicolai.ernst@outlook.de>
2017-12-21 17:57:18 +0100

    Datum dynamisch aus Alexa Request holen

--- index.php
+++ index.php
@@ -8,8 +8,15 @@ require_once('alexa.php');
 $mensa = new MensaClass();
 $alexa = new AlexaClass();
 
+// Request (sent from Alexa)
+$input = file_get_contents('php://input');
+$echoArray = json_decode($input);
+
+// Extract date from request
+$date = $echoArray->request->intent->slots->DateSlot->value;
+
 // Filtered JSON on Date, Location, Category
-$data = $mensa->filter('2017-12-21', 'fulda', 'vegetarisch');
+$data = $mensa->filter($date, 'fulda', 'vegetarisch');
 $plain = $mensa->toPlainText($data);
 
 $json = $alexa->response($plain);
