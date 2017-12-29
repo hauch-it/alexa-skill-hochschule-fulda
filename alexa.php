c52d3dfdc6a7610e8767f80fb4b8d9d0e7e343e5
nicolai92 <nicolai92@protonmail.com>
2017-12-29 14:26:10 +0100

    LaunchRequest soll Beschreibung des Skills vorlesen

--- alexa.php
+++ alexa.php
@@ -21,4 +21,31 @@ class AlexaClass {
         $json = json_encode ( $responseArray );
         return $json;
     }
+    
+    /*
+    	Welcome text, that describes the Skills functionality
+    */
+    public function welcome() {
+    	$welcome = "Willkommen bei der Mensa in Fulda. Um dir vorlesen zu lassen, 
+    	welche Speisen angeboten werden, sage: Frage Mensa Fulda, was heute vegetarisch ist. 
+    	Oder: Frage Mensa Fulda, was morgen vegan ist. Weitere Beispiele findest du in der 
+    	Beschreibung des Skills. Ich hoffe, ich bin Dir nützlich!";
+    }
+    
+    /*
+    	Check, if welcome text should be read or not
+    	@param alexaRequest - Request that is sent by Alexa
+    */
+    public function isLaunchRequest($alexaRequest) {
+    	// Extract from request
+    	$request = $alexaRequest->request->type;
+    	// When LaunchRequest then read welcome text
+    	if ( strcmp ( $request, "LaunchRequest" ) == 0 ) {
+    		return True;
+    	}
+    	// IntentRequest
+    	else {
+    		return false;
+    	} 
+    }
 }
